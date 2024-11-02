e9dc75f2243cc2e4f2f7731a11601d3de4152a48
faisalmohamed <faisalalimohamed@outlook.com>
2024-11-02 11:05:54 -0400

    Initial Commit

---
+++ funfacts/ContentView.swift
@@ -0,0 +1,41 @@
+import SwiftUI
+
+struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme: ColorScheme
+    @State private var funFact: String = funFactsList[.random(in: 2...301)] ?? ""
+    
+    var body: some View {
+                
+        VStack {
+            ZStack {
+                Image(colorScheme == .dark ? "funfacts-dark" : "funfacts")
+                    .resizable()
+                    .scaledToFit()
+            }
+            Text(funFact)
+                .foregroundColor(colorScheme == .dark ? .white : .black)
+                .multilineTextAlignment(.center)
+                .fontWeight(.regular)
+                .fontWidth(.standard)
+                .padding(25)
+            Spacer()
+            Button("Next Fun Fact") {
+                funFact = funFactsList[.random(in: 2...301)] ?? ""
+                print(funFact)
+                
+            }
+            .font(.title)
+            .foregroundColor(.white)
+            .buttonStyle(.borderedProminent)
+        }
+        .font(.title)
+        .foregroundColor(.white)
+        .buttonStyle(.borderedProminent)
+        .cornerRadius(10.0)
+        .padding(.all)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ funfacts/factView.swift
@@ -0,0 +1,6 @@
+struct factView {
+    func factView() {
+        print((funFactsList[.random(in: 02...301)])!)
+        return
+    }
+}
