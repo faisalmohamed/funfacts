08843f3e04d48076bbb340dced01e6ea748af7e9
faisalmohamed <faisalalimohamed@outlook.com>
2024-12-10 18:30:24 -0500

    added share sheet

---
+++ funfacts/ShareSheet.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import UIKit
+
+struct ShareSheet: UIViewControllerRepresentable {
+    var items: [Any]
+    var activities: [UIActivity]? = nil
+
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        let controller = UIActivityViewController(activityItems: items, applicationActivities: activities)
+        return controller
+    }
+
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
+}
